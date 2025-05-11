fbe21fb2a2cebc2d2d3a5807214dc730b117f832
aditya gore <aditya_gore@hotmail.com>
2025-05-11 11:32:06 +0530

    resolved

---
+++ src/main/java/ConcurrentTicketBooking/Api.java
@@ -0,0 +1,8 @@
+package ConcurrentTicketBooking;
+
+public class Api {
+//    @PostMapping("/api/v1/seats/{seatId}/bookings")
+//    public void book(@PathVariable Integer seatId, Integer userId) {
+//        bookSeat(seatId, userId);
+//    }
+}
---
+++ src/main/java/ConcurrentTicketBooking/Seat.java
@@ -0,0 +1,12 @@
+package ConcurrentTicketBooking;
+
+public class Seat {
+    Integer id;
+
+    public Seat(Integer id) {
+        this.id = id;
+    }
+
+    public Seat() {
+    }
+}
